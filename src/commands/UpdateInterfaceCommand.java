26174ff3c5ad24b936f69b04b264e9bad5dfecff
Eli Edrian Tan <eliedrian1996@gmail.com>
2014-12-11 22:11:59 +0800

    additionally added more to updateinterface

--- src/commands/UpdateInterfaceCommand.java
+++ src/commands/UpdateInterfaceCommand.java
@@ -7,6 +7,7 @@ package commands;
 
 import base.Command;
 import base.UI;
+import java.util.Calendar;
 import java.util.Map;
 import javax.swing.JLabel;
 import manager.NoteManager;
@@ -17,20 +18,58 @@ import model.Note;
  * @author tan
  */
 public class UpdateInterfaceCommand extends Command {
-    
+    Map fields;
     public UpdateInterfaceCommand(UI ui){
         this.ui = ui;
+        fields = ui.getFields();
     }
 
     @Override
     public void execute() {
-        Map fields = ui.getFields();
-        JLabel runningTask = (JLabel) fields.get("RunningTaskTitle");
+        updateRunningTaskLabel();
+        updateTodayIsWhat();
+    }
+    
+    /**
+     * updates the RunningTask label.
+     */
+    private void updateRunningTaskLabel(){
         Note latest = NoteManager.getNextNote();
+        JLabel runningTask = (JLabel) fields.get("RunningTaskTitle");
         if(latest==null)
             runningTask.setText("No task!");
         else
             runningTask.setText(latest.getTitle());
+    }
+    
+    /**
+     * updates the TodayIsWhat label.
+     */
+    private void updateTodayIsWhat(){
+        JLabel todayIsWhat = (JLabel) fields.get("TodayIsWhat");
+        StringBuilder builder = new StringBuilder("Today is ");
+        builder.append(intToDayOfWeek(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)));
+        todayIsWhat.setText(builder.toString());
+    }
+    
+    private String intToDayOfWeek(int day){
+        switch(day){
+            case 1:
+                return "Sunday";
+            case 2:
+                return "Monday";
+            case 3:
+                return "Tuesday";
+            case 4:
+                return "Wednesday";
+            case 5:
+                return "Thursday";
+            case 6:
+                return "Friday";
+            case 7:
+                return "Saturday";
+        }
+        return null;
     }
     
 }
